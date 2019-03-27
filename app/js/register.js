b12a25a924bbcc8b6c6395f3195b60e3c59ec03d
gb1615420876 <1615420876@qq.com>
2019-03-27 14:39:51 +0800

    模态框

--- app/js/register.js
+++ app/js/register.js
@@ -2,93 +2,18 @@ function isPhoneNo(phone) {
     var pattern = /^1[34578]\d{9}$/;
     return pattern.test(phone);
 }
-
-function Vcode(code) {
-    var $pattern = /^[a-z0-9_-]{6}$/;
-    return $pattern.test(code);
-}
-
-function userTel(inputid, spanid, telephone,btnCode) {
-    $(inputid).click(function () {
-        $(inputid).toggle(function(){
-            $(btnCode).toggle()
-        })
-        
-    });
-    $(telephone).change(function () {
-        $(spanid).html("");
+function userTel(inputid,spanid,telid) {
+    $(inputid).click(function() {
+        if ($.trim($(telid).val()).length == 0) {
+            $(spanid).html(" 手机号没有输入");
+        } else {
+            if (isPhoneNo($.trim($(telid).val())) == false) {
+                $(spanid).html(" 手机号码不正确");
+            }
+        }
+        $(inputid).click(function() {
+            $(spanid).html("");
+        });
     });
 };
-userTel('#btn', "#checkExistPhone", '#telephone',"#btnCode");
-
-function shoot(regid, loginid, codeid, telid, spanid) {
-    $(regid).click(function () {
-        if (isPhoneNo($(telid).val())) {
-            $(loginid).toggle(function () {
-                $(codeid).toggle()
-            })
-        } else {
-            if (isPhoneNo($(telid).val()) == false) {
-                $(spanid).html(" 手机号码格式不正确");
-            }
-            if ($(telid).val().length == 0) {
-                $(spanid).html(" 手机号没有输入");
-            }
-        }
-
-    });
-    $(telid).change(function () {
-        $(spanid).html("");
-    });
-};
-shoot("#reg", ".login-box", ".vCode-box", '#telephone', "#checkExistPhone");
-
-function pidding(nextone, vCodebox, pwdbox, Vword, spanid) {
-    $(nextone).click(function () {
-        if (Vcode($(Vword).val())) {
-            $(vCodebox).toggle(function () {
-                $(pwdbox).toggle()
-            })
-        } else {
-            if (Vcode($(Vword).val()) == false) {
-                $(spanid).html("验证码不正确");
-            }
-            if ($(Vword).val().length == 0) {
-                $(spanid).html("输入验证码");
-            }
-        }
-    });
-
-}
-pidding("#next-one", ".vCode-box", ".pwd-box", ".vCode-word", "#checkExistCode")
-
-$("#reg").click(function () {
-    $(".tel").html($("#telephone").val());
-})
-$("#vCode-word").click(function () {
-    var time = 60;
-    var timer = setInterval(function () {
-        time--;
-        $("#vCode-word").text("" + time + "秒重新获取");
-        if (time == 0) {
-            clearInterval(timer);
-            $("#vCode-word").text("获取验证码");
-        }
-    }, 1000);
-});
-// var $form = document.querySelector('.form-box');
-// $form['telephone'].onchange = function () {
-//     var telephone = this.value;
-//     var obj = {
-//         telephone
-//     }
-//     sendAjax('../server/Verification.php', 'get', obj, data => {
-//         // 所期待的data应该是一个正确的json字符串
-//         data = JSON.parse(data);
-//         if (data.code == 200) {
-//             alert('用户名可用');
-//         } else {
-//             alert('用户名不可用');
-//         }
-//     });
-// }
+userTel('#btn', "#checkExistPhone",'#telephone');
--- server/login.php
+++ server/login.php
@@ -2,11 +2,10 @@
 $tel=$_POST["telephone"];
 $pwd=$_POST["pwd"];
 $coon=mysqli_connect("localhost","root","","k");
-$selectsql="select * from tel";
 $result=mysqli_query($coon,$selectsql);
 if($result){
-    echo"<script>alert('登录成功');location.href='../app/home.html';</script>";
+    echo"<script>alert('登录成功');location.href='../app/index.html';</script>";
 }else{
-    echo"<script>alert('登录失败');location.href='../app/home.html';</script>";
+    echo"<script>alert('登录失败');location.href='../app/login.html';</script>";
 }
 ?>
